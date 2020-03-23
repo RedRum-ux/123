0e3de7cf152572b9b33ec69019cd04e93e2f7b08
Никита Москвин <nik_moskvin_2014@mail.ru>
2020-03-23 23:06:04 +0300

    123

---
+++ ConsoleApplication1/ConsoleApplication1/Count.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Count
+    {
+        public string Name { get; set; }
+        public int Num { get; set; }
+        public double Sum { get; set; }
+        public void add()
+        {
+            Name = Console.ReadLine();
+            Num = Convert.ToInt32(Console.ReadLine());
+            Sum = Convert.ToDouble(Console.ReadLine());
+        }
+        public void watch()
+        {
+            Console.Write(" " + Name);
+            Console.Write(" " + Num);
+            Console.Write(" " + Sum);
+        }
+    }
+}
